c063524b3704539def439236fedebaf559743c45
cicaf <womarow@gmail.com>
2025-04-03 18:17:53 +0300

    Part 2

--- submission/02.sh
+++ submission/02.sh
@@ -2,18 +2,23 @@
 
 # Create a native segwit address and get the public key from the address.
 #wallet name
-WALLET_NAME="btrustwallet"
+#WALLET_NAME="btrustwallet"
 
 #Wallet loading
-bitcoin-cli -regtest loadwallet "$WALLET_NAME" >/dev/null 2>&1
+#bitcoin-cli -regtest loadwallet "$WALLET_NAME" >/dev/null 2>&1
 
 # generating a bech32 address
-ADDRESS=$(bitcoin-cli -regtest getnewaddress "" bech32)
+#ADDRESS=$(bitcoin-cli -regtest getnewaddress "" bech32)
 
 # Extracting the public key for  address
+#PUBKEY=$(bitcoin-cli -regtest getaddressinfo "$ADDRESS" | jq -r '.pubkey')
+
+#####
+#Gen a native Segwit (Bech32)
+ADDRESS=$(bitcoin-cli -regtest getnewaddress "" bech32)
+
+#Get pub key assoc with the address
 PUBKEY=$(bitcoin-cli -regtest getaddressinfo "$ADDRESS" | jq -r '.pubkey')
 
-#Finula
-echo "Native SegWit Address: $ADDRESS"
-echo "Public Key: $PUBKEY"
-
+#Println Pubkey
+echo "$PUBKEY"
