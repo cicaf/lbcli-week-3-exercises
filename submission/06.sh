61f840bdc103fbbb766a87ca96cbb659e416ae02
cicaf <womarow@gmail.com>
2025-04-03 19:54:48 +0300

    Add number 6

--- submission/06.sh
+++ submission/06.sh
@@ -1,2 +1,16 @@
 # What is the hash of this partially signed transaction?
 # transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+
+#signed transactions
+PSBT="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
+
+#PSBT and output
+output=$(bitcoin-cli -regtest -named decodepsbt psbt=$PSBT)
+
+#txid and hash on output
+txid=$(echo "$output" | jq -r '.tx.txid')
+hash=$(echo "$output" | jq -r '.tx.hash')
+
+#txid and hash...
+#txid"
+echo "$hash"
